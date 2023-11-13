c91e492b2de094babb27b7d2fdcb360df80ccfe6
Shital <shital.subhash.patil@accenture.com>
2023-11-13 19:49:22 +0530

    contact update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,29 @@
 import UserContext from '../utils/UserContext';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 const Contact = () => {
     const result = useContext(UserContext);
+
+const [longgestNonRepeatingChars , setLonggestNonRepeatingChars ] = useState('ABCDDDEFGHI');
+    const findTheLonggestNonRepeatingChars =() => {
+  const arr = [];
+
+  
+  longgestNonRepeatingChars.split('').forEach((e, index)=>{
+    
+    console.log(longgestNonRepeatingChars.indexOf(e), index)
+    if(longgestNonRepeatingChars.indexOf(e) !== index){
+     arr.length = 0
+       arr.push(e)
+    } else {
+      
+       arr.push(e)
+    }
+    
+  })
+  setLonggestNonRepeatingChars(arr);
+
+}
     return (
         <>
             <h1>Contact US</h1>
@@ -16,6 +37,12 @@ const Contact = () => {
                 Mobile Number <input type="text" placeholder='Number' className='rounded-md border-2' />
                 <button className="rounded-md border-solid shadow-lg mx-3 px-3 bg-blue-200">Submit</button>
             </form>
+            <div>
+                <input className='rounded-md border-2'  value={longgestNonRepeatingChars} onChange={(e)=>{setLonggestNonRepeatingChars(e.target.value)}}/>
+                <button onClick={()=>{findTheLonggestNonRepeatingChars()}} className="rounded-md border-solid shadow-lg mx-3 px-3 bg-blue-200"> Check longgest non repeating chars</button>
+
+                <p>{longgestNonRepeatingChars}</p>
+            </div>
         </>
     )
 }
